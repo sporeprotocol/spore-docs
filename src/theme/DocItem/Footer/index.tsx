dcf833d3e6a466c6b1ae468e22fa0f41f5d70ddb
ShookLyngs <shooklyngs@gmail.com>
2023-09-19 03:11:56 +0800

    style: improve page footer styles

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "@theme-original/DocItem/Footer";
 import Feedback from "../../../components/Feedback";
+import PageEnd from "../../../components/PageEnd";
 
 export default function FooterWrapper(props: any) {
   return (
     <>
+      <PageEnd />
       <Feedback />
       <Footer {...props} />
     </>
