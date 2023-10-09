187874763e27997434db4ad60a6005eed6dfbe97
anni <112425356+anipenguin@users.noreply.github.com>
2023-10-09 08:33:34 -0700

    Update docusaurus.config.js
    enable KaTeX

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -27,6 +29,8 @@ const config = {
       ({
         docs: {
           routeBasePath: '/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
           breadcrumbs: false,
           showLastUpdateTime: true,
           showLastUpdateAuthor: false,
@@ -105,6 +109,16 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 };
 
 module.exports = config;
