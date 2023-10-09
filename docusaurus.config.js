e4cf4e882d25b50c4c8ede9aa39191271cdf6198
anni <112425356+anipenguin@users.noreply.github.com>
2023-10-09 09:16:51 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,6 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -29,8 +27,6 @@ const config = {
       ({
         docs: {
           routeBasePath: '/',
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
           breadcrumbs: false,
           showLastUpdateTime: true,
           showLastUpdateAuthor: false,
@@ -110,15 +106,7 @@ const config = {
       },
     }),
 
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
+
 };
 
 module.exports = config;
